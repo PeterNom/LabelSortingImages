95256e65895d942d7a80fe14c798f14be22d41cd
Panagiotis Nomikos <peternomik@gmail.com>
2023-10-23 18:53:48 +0300

    Added basic menu.

--- LabelSortingImages/Program.cs
+++ LabelSortingImages/Program.cs
@@ -10,5 +10,59 @@ string path = "C:\\Users\\peter\\Pictures\\Saved Pictures\\photos October_new";/
 AddImagesSorted addImages = new(path);
 
 //addImages.renameSortedImages();
-addImages.addImages("AAAC");
 
+
+bool showMenu = true;
+while (showMenu)
+{
+    showMenu = MainMenu();
+}
+
+bool MainMenu()
+{
+    Console.Clear();
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("1) Rename all images");
+    Console.WriteLine("2) Add images at a specific index");
+    Console.WriteLine("3) Exit");
+    Console.Write("\r\nSelect an option: ");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            RenameAll();
+            return true;
+        case "2":
+            EnterByIndex();
+            return true;
+        case "3":
+            return false;
+        default:
+            return true;
+    }
+}
+
+string CaptureInput()
+{
+    return Console.ReadLine();
+}
+
+void EnterByIndex()
+{
+    Console.Clear();
+    Console.WriteLine("Enter the path of the images.");
+    path = CaptureInput();
+
+    Console.Clear();
+    Console.WriteLine("Enter index.");
+    addImages.addImages(CaptureInput());
+}
+
+void RenameAll()
+{
+    Console.Clear();
+    Console.WriteLine("Enter the path of the images.");
+    path = CaptureInput();
+
+    addImages.renameSortedImages();
+}
